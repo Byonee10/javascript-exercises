ae9b867bd96db5dd92aaf72eae58f394c54455c4
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-08-18 22:59:03 +0300

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if(n>0){
+        if(n == 1 || n==2){
+            return 1;
+        }
+    
+        return fibonacci(n-1)+fibonacci(n-2);s
+    }
+    else return "OOPS";
+   
 };
 
 // Do not edit below this line
