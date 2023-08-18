1072514b98d950868161f344cba04319730ce6db
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-08-18 21:28:14 +0300

    Solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+  let array = [...arg];
+	if(array.length==0){
+    return 0;
+  }
+  let sum = 0;
+  for(let i = 0;i<array.length;i++){
+    sum+=array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+  let nums = [...arg];
+  return nums.reduce((multiplication,num) => multiplication*=num,1);
+  
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n==1 || n == 0){
+    return 1;
+  }
+
+  return n*factorial(n-1);
 };
 
 // Do not edit below this line
