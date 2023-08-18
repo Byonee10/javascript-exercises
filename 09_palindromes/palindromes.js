d9d6fad3d59d62b339f9384559daa2d2ec95899c
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-08-18 22:53:53 +0300

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let listedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "").split("");
+    console.log(listedStr);
+    let len = Math.floor(listedStr.length/2);
+    for(let i = 0; i<len;i++){
+        if(listedStr[i] !== listedStr[listedStr.length-i-1]){
+            return false;
+        }
+        
+    }
+    return true;
 
-};
+
+};  
 
 // Do not edit below this line
 module.exports = palindromes;
